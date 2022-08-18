bdb94d308251fe9021407589a399c148c220966a
Juan S. Marquerie <jsmarquerie@gmail.com>
2022-08-18 20:59:08 +0200

    Added todo list

--- src/render.h
+++ src/render.h
@@ -7,6 +7,18 @@
 #include <glm/vec3.hpp>
 #include <glm/gtc/quaternion.hpp>
 
+/**
+ * A Wrapper for the rendering backend, for now with webgl
+ * TODO:
+ *  1) Basic VAO, VBO functionality
+ *  2) Add perspective
+ *  2) Basic I/O of meshes
+ *  3) Basic I/O of textures
+ *  4) Scene representation
+ *  4) GLTF import
+ *  5) Culling (VR culling??)
+ *  5) Render graph..?
+ * */
 namespace Render {
 
     struct sRenderCall {
