e70d2048784e7ffc6d680ca2de6056d5f6233738
Juan S. Marquerie <jsmarquerie@gmail.com>
2022-08-19 12:01:01 +0200

    Started the more complex rendering

--- src/render.cpp
+++ src/render.cpp
@@ -1,21 +1,42 @@
 #include "render.h"
 
+#include <GLES3/gl3.h>
 #include <emscripten/emscripten.h>
 #include <emscripten/html5_webgl.h>
 #include <emscripten/html5.h>
 #include <webgl/webgl2.h>
 
 
-void Render::render_frame() {
+void Render::sInstance::render_frame(const glm::mat4x4 &view_proj_frame) {
     int width, height, lef, right;
     emscripten_get_canvas_element_size("#canvas", &width, &height);
 
     glViewport(0, 0, width, height);
     glClearColor(0.0, 0.0, 0.0, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glm::mat4x4 model;
+    for(uint16_t i = 0; i < draw_stack_size; i++) {
+        sDrawCall &draw_call = draw_stack[i];
+        sShader &shader = shaders[draw_call.shader_id];
+        sMeshBuffers &mesh = meshes[draw_call.mesh_id];
+
+        model = draw_call.transform.get_model();
+
+        shader.activate();
+
+        glBindVertexArray(mesh.VAO);
+
+        shader.set_uniform_matrix4("u_model_mat", model);
+        shader.set_uniform_matrix4("u_vp_mat", view_proj_frame);
+
+        if (mesh.is_indexed) {
+
+        } else {
+            glDrawArrays(mesh.primitive, 0, mesh.primitive_count);
+        }
+    }
     //glUseProgram(shader_program);
     //glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-
-    glDrawArrays(GL_TRIANGLES, 0, 3);
 
 }
--- src/render.h
+++ src/render.h
@@ -7,6 +7,12 @@
 #include <glm/vec3.hpp>
 #include <glm/gtc/quaternion.hpp>
 
+#include "transform.h"
+#include "shader.h"
+
+#define SHADER_TOTAL_COUNT 10
+#define MESH_TOTAL_COUNT 20
+#define DRAW_CALL_STACK_SIZE 30
 /**
  * A Wrapper for the rendering backend, for now with webgl
  * TODO:
@@ -21,16 +27,35 @@
  * */
 namespace Render {
 
-    struct sRenderCall {
+    // TODO: Parenting
+    struct sDrawCall {
+        sTransform transform;
+        uint8_t mesh_id;
+        uint8_t shader_id;
+    };
 
+    struct sMeshBuffers {
+        uint32_t VAO;
+        uint32_t VBO;
+        uint32_t UBO;
+
+        uint32_t primitive_count;
+        uint32_t primitive;
+
+        bool is_indexed = false;
     };
     
     struct sInstance {
+        sShader shaders[SHADER_TOTAL_COUNT];
+        sMeshBuffers meshes[MESH_TOTAL_COUNT];
+
+        uint16_t draw_stack_size = 0;
+        sDrawCall draw_stack[DRAW_CALL_STACK_SIZE];
+
+        void render_frame(const glm::mat4x4 &view_proj_mat);
 
     };
 
-
-    void render_frame();
 };
 
 #endif // RENDER_H_
