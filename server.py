3c8bd2dbc061321a69958735ba9c6f76b2a8ffc9
Juan S. Marquerie <jsmarquerie@gmail.com>
2022-09-02 12:54:32 +0200

    Removed raw volumes & updated server.py

--- server.py
+++ server.py
@@ -16,7 +16,7 @@ server_address = ('', 4443)
 httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket(httpd.socket,
                                server_side=True,
-                               certfile="server.pem",
-                               keyfile="key.pem",
+                               certfile="../server.pem",
+                               keyfile="../key.pem",
                                ssl_version=ssl.PROTOCOL_TLS)
 httpd.serve_forever()
